657a71d670287544f9a8683f88a1df1ee6aa1f1f
GubbyLady <13828962838@163.com>
2023-05-20 14:58:14 +0800

    feat: base_context1.0

---
+++ serves/base_context/components/non_separation/drivers/monitor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/21/10:05
+# @Author  : 周伟帆
+# @Project : log2.0
+# @File    : monitor
+# @Software: PyCharm
+import time
+
+def Init():
+    pass
